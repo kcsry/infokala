663216393ec70f459cf9b6475559619e57826804
Santtu Pajukanta <santtu@pajukanta.fi>
2015-06-01 22:11:45 +0300

    release 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def requirements(filename):
 
 setup(
     name='infokala',
-    version='0.0.1',
+    version='0.0.2',
     description='Info log management system for Desucon and Tracon',
     author='Santtu Pajukanta',
     author_email='japsu@desucon.fi',
