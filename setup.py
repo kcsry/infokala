2367980e4c5539779649b100a73af95f7977b2fc
Santtu Pajukanta <santtu@pajukanta.fi>
2015-11-28 10:45:00 +0200

    get version from package.json

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
+import json
 import os
 from setuptools import setup
 
+
+def mkpath(*parts):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), *parts))
+
+
 def requirements(filename):
-    with open(os.path.abspath(os.path.join(os.path.dirname(__file__), filename))) as f:
+    with open(filename) as f:
         lines = (line.strip() for line in f)
         return [line for line in lines if line and not line.startswith('#')]
 
+
+with open(mkpath('package.json')) as package_json:
+    VERSION = json.loads(package_json.read())['version']
+
+
 setup(
     name='infokala',
-    version='0.1.0',
+    version=VERSION,
     description='Info log management system for Desucon and Tracon',
     author='Santtu Pajukanta',
     author_email='japsu@desucon.fi',
@@ -19,5 +30,5 @@ setup(
     package_data={'infokala': ['static/infokala/*']},
     include_package_data=True,
     zip_safe=False,
-    install_requires=requirements('requirements.txt'),
+    install_requires=requirements(mkpath('requirements.txt')),
 )
