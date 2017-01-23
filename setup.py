3cc0bc47fbec7a8b5ce1c46fa5d78c88259bcf18
Aarni Koskela <akx@iki.fi>
2017-01-23 13:45:30 +0200

    Remove file system reading shenanigans from setup.py
    They won't work correctly when installing from an sdist zip

--- setup.py
+++ setup.py
@@ -1,27 +1,10 @@
 #!/usr/bin/env python
 
-import json
-import os
 from setuptools import setup
-
-
-def mkpath(*parts):
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), *parts))
-
-
-def requirements(filename):
-    with open(filename) as f:
-        lines = (line.strip() for line in f)
-        return [line for line in lines if line and not line.startswith('#')]
-
-
-with open(mkpath('package.json')) as package_json:
-    VERSION = json.loads(package_json.read())['version']
-
 
 setup(
     name='infokala',
-    version=VERSION,
+    version='0.3.1',
     description='Info log management system for Desucon and Tracon',
     author='Santtu Pajukanta',
     author_email='japsu@desucon.fi',
@@ -30,5 +13,5 @@ setup(
     package_data={'infokala': ['static/infokala/*']},
     include_package_data=True,
     zip_safe=False,
-    install_requires=requirements(mkpath('requirements.txt')),
+    install_requires=['Django>=1.7', 'python-dateutil>=2.4.2'],
 )
