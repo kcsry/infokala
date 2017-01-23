857cdc71557dcc653f8053dd8093d225884d8690
Aarni Koskela <akx@iki.fi>
2017-01-23 14:45:05 +0200

    Become version 0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='infokala',
-    version='0.3.1',
+    version='0.4',
     description='Info log management system for Desucon and Tracon',
     author='Santtu Pajukanta',
     author_email='japsu@desucon.fi',
