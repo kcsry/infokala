6c1ee3d4df3dd613cf5bb66f58cfeffe0fe9a8d8
Aarni Koskela <akx@iki.fi>
2017-07-24 11:43:53 +0300

    Emreactening, step 2: data-bind replacements (see below)
    * Event handlers are stubs
    * Best guesses have been made as to the scope of the various props
    * Input value binds have NOT been rewritten yet
    * Some of the `hidden` hacks should be replaced by just eliding the entire component if necessary

--- infokala/static_src/infokala/InfokalaApp.jsx
+++ infokala/static_src/infokala/InfokalaApp.jsx
@@ -1,27 +1,27 @@
-/* eslint-disable max-len, react/prefer-stateless-function */
+/* eslint-disable max-len, react/prefer-stateless-function, react/prop-types, no-alert, jsx-a11y/no-static-element-interactions */
 import React from 'react';
+import cx from 'classnames';
+
 import loaderImage from './loader.svg';
 
-const NavBar = () => (
+const NavBar = ({ config, messageTypeFilters = [], activeFilter }) => (
   <div className="navbar navbar-default navbar-fixed-top">
     <div className="container">
-      <div className="navbar-header"><a href="href" className="navbar-brand">Infokala</a></div>
-      <div
-        id="base-navbar-collapse"
-        data-bind="visible: true"
-        className="collapse navbar-collapse"
-      >
-        <ul data-bind="foreach: messageTypeFilters" className="nav navbar-nav">
-          <li data-bind="css: { active: slug === $parent.activeFilter().type }">
-            <a href="href" data-bind="text: name, click: $parent.setFilterType">...</a>
-          </li>
+      <div className="navbar-header">
+        <a href="href" className="navbar-brand">Infokala</a>
+      </div>
+      <div id="base-navbar-collapse" className="collapse navbar-collapse">
+        <ul className="nav navbar-nav">
+          {messageTypeFilters.map(({ slug, name, href = '#' }) => (
+            <li className={cx({ active: slug === activeFilter.type })} key={slug}>
+              <a href={{ href }} onClick={() => alert(`setFilterType ${slug}`)}>{name}</a>
+            </li>
+          ))}
         </ul>
         <ul className="nav navbar-nav navbar-right">
           <li>
-            <a href="href" data-bind="attr: { href: config.logoutUrl }">
-              Kirjaa
-              <b data-bind="text: config.user.displayName">...</b>
-              ulos</a>
+            <a href={config.logoutUrl}>
+              Kirjaa <b>{config.user.displayName}</b> ulos</a>
           </li>
         </ul>
       </div>
@@ -29,24 +29,32 @@ const NavBar = () => (
   </div>
 );
 
-const FilterRow = () => (
-  <div data-bind="visible: shouldShowFilters()" className="container">
+const FilterRow = ({ shouldShowFilters = true, messageStateSpecialFilters = [], messageStateFilters = [], activeFilter }) => (
+  <div className={cx({ hidden: !shouldShowFilters, container: true })}>
     <div className="infokala-filter-row">
       <div className="row"><strong className="col-md-1">Suodata</strong>
         <div className="col-md-11 btn-toolbar">
-          <div data-bind="foreach: messageStateSpecialFilters" className="btn-group btn-group-sm">
-            <button
-              data-bind="text: name, click: $parent.setFilterState, css: { &quot;btn-primary&quot;: $data === $parent.activeFilter().state }"
-              className="btn btn-default"
-            >...
-            </button>
+          <div className="btn-group btn-group-sm">
+            {messageStateSpecialFilters.map(({ name }) => (
+              <button
+                key={name}
+                onClick={() => alert('setFilterState')}
+                className={cx('btn btn-default', { 'btn-primary': activeFilter.name === name })}
+              >
+                {name}
+              </button>
+            ))}
           </div>
-          <div data-bind="foreach: messageStateFilters" className="btn-group btn-group-sm">
-            <button
-              data-bind="text: name, click: $parent.setFilterState, css: { &quot;btn-primary&quot;: $data === $parent.activeFilter().state }"
-              className="btn btn-default"
-            >...
-            </button>
+          <div className="btn-group btn-group-sm">
+            {messageStateFilters.map(({ name }) => (
+              <button
+                key={name}
+                onClick={() => alert('setFilterState')}
+                className={cx('btn btn-default', { 'btn-primary': activeFilter.name === name })}
+              >
+                {name}
+              </button>
+            ))}
           </div>
         </div>
       </div>
@@ -55,7 +63,7 @@ const FilterRow = () => (
 );
 
 const LoadingSpinner = () => (
-  <div data-bind="visible: isLoading" className="container">
+  <div className="container">
     <div className="row">
       <div className="col-md-12">
         <img alt="Loading" src={loaderImage} />
@@ -64,9 +72,9 @@ const LoadingSpinner = () => (
   </div>
 );
 
-const SubmitMessage = () => (
-  <div style={{ display: 'none' }} data-bind="visible: !isLoading()" className="container">
-    <form data-bind="submit: sendMessage" className="infokala-message-form">
+const SubmitMessage = ({ shouldShowNewMessageWarning = true, shouldShowMessageType = true, visibleMessageTypes = [] }) => (
+  <div className="container">
+    <form onSubmit={() => alert('sendMessage')} className="infokala-message-form">
       <div className="row">
         <div className="col-md-2 col-sm-3 infokala-author-col">
           <input
@@ -78,8 +86,11 @@ const SubmitMessage = () => (
           />
         </div>
         <div
-          data-bind="css: { &quot;col-md-7&quot;: shouldShowMessageType(), &quot;col-md-9&quot;: !shouldShowMessageType(), &quot;infokala-message-col-middle&quot;: shouldShowMessageType() }"
-          className="infokala-message-col col-sm-9"
+          className={cx('infokala-message-col col-sm-9', {
+            'col-md-7': shouldShowMessageType,
+            'col-md-9': !shouldShowMessageType,
+            'infokala-message-col-middle': shouldShowMessageType,
+          })}
         >
           <input
             data-bind="value: message"
@@ -90,144 +101,163 @@ const SubmitMessage = () => (
           />
         </div>
         <div
-          data-bind="visible: shouldShowMessageType()"
-          className="col-md-2 col-sm-6 col-xs-9 infokala-message-type-col"
+          className={cx('col-md-2 col-sm-6 col-xs-9 infokala-message-type-col', { hidden: !shouldShowMessageType })}
         >
-          <select
-            data-bind="options: visibleMessageTypes, optionsText: &quot;name&quot;, optionsValue: &quot;slug&quot;, value: manualMessageType"
-            className="form-control"
-          />
+          <select data-bind="value: manualMessageType" className="form-control">
+            {visibleMessageTypes.map(mt => <option value={mt.slug}>{mt.name}</option>)}
+          </select>
         </div>
         <div className="col-md-1 col-sm-6 col-xs-3">
           <input type="submit" value="Luo" className="btn btn-primary" />
         </div>
       </div>
-      <div data-bind="visible: shouldShowNewMessageWarning" className="row infokala-new-message-warning">
+      <div className={cx('row infokala-new-message-warning', { hidden: !shouldShowNewMessageWarning })}>
         <div className="col-md-1" />
-        <div className="col-md-11 text-muted">Huomaathan, että uusi viestisi ei näy nykyisillä
-          suodatinasetuksilla.
+        <div className="col-md-11 text-muted">
+          Huomaathan, että uusi viestisi ei näy nykyisillä suodatinasetuksilla.
         </div>
       </div>
     </form>
   </div>
 );
 
-const MessageHistory = () => (
-  <div data-bind="visible: isMessageOpen" className="infokala-message-history">
-    <div className="row">
-      <div className="col-md-1" />
-      <div className="col-md-11 h5 infokala-history-header">Kommentoi</div>
-    </div>
-    <div className="row infokala-comment-form">
-      <div className="col-md-1" />
-      <div
-        data-bind="text: (new Date()).toLocaleTimeString(&quot;fi-FI&quot;).replace(/\./g, &quot;:&quot;)"
-        className="col-md-1 infokala-timestamp"
-      />
-      <div data-bind="text: $parent.author() + &quot;:&quot;" className="col-md-1" />
-      <form
-        data-bind="submit: handleComment, disabled: isCommentPending, click: function(){}, clickBubble: false"
-        className="col-md-8"
-      >
-        <input
-          type="text"
-          data-bind="textInput: newComment"
-          className="form-control infokala-comment-field"
-        />
-      </form>
-    </div>
-    <div className="row">
-      <div className="col-md-1" />
-      <div className="col-md-11 h5 infokala-history-header">Historia</div>
-    </div>
-    <div data-bind="foreach: eventList" className="infokala-message-event-container">
-      <div className="row infokala-message-event">
+const MessageHistory = ({ $parent, eventList, isCommentPending, isMessageOpen }) => {
+  if (!isMessageOpen) return null;
+  return (
+    <div className="infokala-message-history">
+      <div className="row">
         <div className="col-md-1" />
-        <div data-bind="text: time" className="col-md-1 infokala-timestamp" />
-        <div data-bind="text: author + &quot;:&quot;" className="col-md-1" />
-        <div data-bind="html: html, css: classes" className="col-md-9" />
+        <div className="col-md-11 h5 infokala-history-header">Kommentoi</div>
       </div>
-    </div>
-  </div>
-);
-
-const MessageRow = message => (
-  <div
-    data-bind="attr: {id: &quot;infokala-message-&quot; + id, &quot;class&quot;: &quot;infokala-message infokala-msgtype-&quot; + messageType.slug}, css: {&quot;infokala-message-cycleable&quot;: isCycleable, &quot;infokala-message-deleted&quot;: isDeleted, &quot;infokala-open&quot;: isMessageOpen}"
-  >
-    <div className="row infokala-action-row">
-      <div className="col-md-12 text-muted">
-        <div data-bind="visible: isEditable" className="pull-right"><span
-          data-bind="visible: isCycleable(), click: cycleMessageState"
-          className="infokala-state-switcher"
+      <div className="row infokala-comment-form">
+        <div className="col-md-1" />
+        <div
+          className="col-md-1 infokala-timestamp"
+        >{(new Date()).toLocaleTimeString('fi-FI').replace(/\./g, ':')}</div>
+        <div className="col-md-1">{$parent.author}:</div>
+        <form
+          onSubmit={() => alert('handleComment')}
+          disabled={isCommentPending}
+          className="col-md-8"
         >
-          <span data-bind="text: state().name, css: state().labelClass" className="label" />
-          <span
-            data-bind="with: nextState()"
-            className="infokala-next-state"
-          >
-            <span className="text-muted">&thinsp;&rArr;&thinsp;</span>
-            <span data-bind="text: name, css: labelClass" className="label" /></span></span>&emsp;
-          <span
-            data-bind="visible: $parent.shouldShowMessageType(), text: messageType.name"
-            className="label label-default"
-          />&emsp;<span
-            data-bind="visible: commentAmount() &gt; 0, click: toggleOpen"
-            className="infokala-comments-button"
-          ><span
-            data-bind="html: &quot;&amp;#x1f4ac; &quot; + commentAmount()"
-            className="btn btn-xs btn-info"
-          />&emsp;</span><span
-            data-bind="click: toggleEdit, clickBubble: false"
-            className="btn btn-xs btn-primary infokala-edit-message-button"
-          >&#x270e;</span>&emsp;<span
-            data-bind="click: deleteMessage, clickBubble: false"
-            className="btn btn-xs btn-danger infokala-delete-message-button"
-          >&times;</span></div>
-        <div data-bind="text: formattedTime" className="small" />
+          <input
+            type="text"
+            data-bind="textInput: newComment"
+            className="form-control infokala-comment-field"
+          />
+        </form>
+      </div>
+      <div className="row">
+        <div className="col-md-1" />
+        <div className="col-md-11 h5 infokala-history-header">Historia</div>
+      </div>
+      <div className="infokala-message-event-container">
+        {eventList.map(({ time, author, classes, id, html }) => (
+          <div className="row infokala-message-event" key={id}>
+            <div className="col-md-1" />
+            <div className="col-md-1 infokala-timestamp">{time}</div>
+            <div className="col-md-1">{author}:</div>
+            <div className={cx('col-md-9', classes)}>{html}</div>
+          </div>
+        ))}
       </div>
     </div>
+  );
+};
+
+const MessageRow = ({ message, nextState, isCycleable, isDeleted, isEditable, isMessageOpen, shouldShowMessageType, isEditPending, isEditingOpen, displayMessage }) => {
+  const { id, messageType, state, author, commentAmount, formattedTime } = message;
+  return (
     <div
-      data-bind="css: {&quot;infokala-inactive&quot;: !state().active}, click: messageType.internal ? null : toggleOpen"
-      className="row infokala-message-text"
+      id={`infokala-message-${id}`}
+      className={cx(
+        `infokala-message infokala-msgtype-${messageType.slug}`,
+        {
+          'infokala-message-cycleable': isCycleable,
+          'infokala-message-deleted': isDeleted,
+          'infokala-open': isMessageOpen,
+        }
+      )}
     >
-      <strong data-bind="text: author" className="col-md-1 infokala-decoration" />
+      <div className="row infokala-action-row">
+        <div className="col-md-12 text-muted">
+          <div className={cx('pull-right', { hidden: !isEditable })}>
+            <span
+              onClick={() => alert('cycleMessageState')}
+              className={cx('infokala-state-switcher', { hidden: !isCycleable })}
+            >
+              <span className={`label ${state.labelClass}`}>{state.name}</span>
+              <span className="infokala-next-state">
+                <span className="text-muted">&thinsp;&rArr;&thinsp;</span>
+                <span className={`label ${nextState.labelClass}`}>{nextState.name}</span>
+              </span>
+            </span>
+            &emsp;
+            <span className={cx('label label-default', { hidden: !shouldShowMessageType })}>{ messageType.name }</span>
+            &emsp;
+            <span
+              onClick={() => alert('toggleOpen')}
+              className={cx('infokala-comments-button', { hidden: commentAmount === 0 })}
+            >
+              <span className="btn btn-xs btn-info">&#x1f4ac;{commentAmount}</span>
+              &emsp;
+            </span>
+            <span
+              onClick={() => alert('toggleEdit')}
+              className="btn btn-xs btn-primary infokala-edit-message-button"
+            >&#x270e;</span>
+            &emsp;
+            <span
+              onClick={() => alert('deleteMessage')}
+              className="btn btn-xs btn-danger infokala-delete-message-button"
+            >&times;</span>
+          </div>
+          <div className="small">{formattedTime}</div>
+        </div>
+      </div>
       <div
-        data-bind="html: displayMessage, visible: !isEditingOpen(), css: {&quot;infokala-edit-pending&quot;: isEditPending}"
-        className="col-md-9"
-      />
-      <form data-bind="submit: handleEdit" className="col-md-9">
-        <input
-          type="text"
-          data-bind="textInput: newText, visible: isEditingOpen, click: function(){}, clickBubble: false, event: {blur: handleEdit}"
-          className="form-control infokala-edit-field"
-        />
-      </form>
+        onClick={() => alert('toggleOpen')}
+        className={cx('row infokala-message-text', { 'infokala-inactive': !state.active })}
+      >
+        <strong className="col-md-1 infokala-decoration">{author}</strong>
+        <div
+          className={cx('col-md-9', { 'infokala-edit-pending': isEditPending, hidden: isEditingOpen })}
+        >{displayMessage}</div>
+        <form onSubmit={() => alert('handleEdit')} className="col-md-9">
+          <input
+            type="text"
+            onBlur={() => alert('handleEdit')}
+            data-bind="textInput: newText"
+            className={cx('form-control infokala-edit-field', { hidden: !isEditingOpen })}
+          />
+        </form>
+      </div>
+      <MessageHistory message={message} />
     </div>
-    <MessageHistory message={message} />
-
-  </div>
-
-);
+  );
+};
 
 export default class InfokalaApp extends React.Component {
   render() {
+    const config = { user: { displayName: 'uguu' } };
+    const message = {
+      id: 1,
+      messageType: { slug: 'x', name: 'x' },
+      state: { labelClass: 'x' },
+    };
+    const nextState = {labelClass: 'y'};
     return (
       <div>
-        <NavBar />
+        <NavBar config={config} />
         <div className="infokala-push-top">
           <FilterRow />
           <LoadingSpinner />
-          <div
-            data-bind="visible: !isLoading()"
-            className="container"
-          >
-            {[{ id: 1 }].map(message => <MessageRow message={message} key={message.id} />)}
+          <div className="container">
+            {[message].map(message => <MessageRow message={message} key={message.id} nextState={nextState} />)}
           </div>
           <SubmitMessage />
         </div>
       </div>
     );
   }
-
 }
