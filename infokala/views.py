fd82e76ea50646c3f272521925f3af946162fec3
Aarni Koskela <akx@iki.fi>
2022-02-22 16:49:27 +0200

    force_text -> force_str

--- infokala/views.py
+++ infokala/views.py
@@ -5,7 +5,7 @@ from itertools import chain
 from django.conf import settings
 from django.http import HttpResponse
 from django.urls import reverse
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 from django.utils.timezone import now
 from django.views.generic import View
 
@@ -142,7 +142,7 @@ class ApiView(View):
             )
 
         try:
-            data = json.loads(force_text(request.body))
+            data = json.loads(force_str(request.body))
         except ValueError:
             return HttpResponse(
                 json.dumps(dict(JSON_BAD_REQUEST, reason='document body is not valid JSON')),
