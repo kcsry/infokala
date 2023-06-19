abb68b465cc38a675d1ca9431ca43dcf197aa457
Aarni Koskela <akx@iki.fi>
2023-06-19 16:11:52 +0300

    Make warnings strict, fix what we can

--- infokala_test_site/settings.py
+++ infokala_test_site/settings.py
@@ -50,7 +50,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 LANGUAGE_CODE = 'fi-fi'
 TIME_ZONE = 'Europe/Helsinki'
 USE_I18N = True
-USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
